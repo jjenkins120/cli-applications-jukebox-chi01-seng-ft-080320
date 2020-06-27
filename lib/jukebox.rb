645777aae477103c859fc589d9af4d20569c3ec5
Jeff Jenkins <jjenkins120@gmail.com>
2020-06-27 23:45:20 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -8,10 +8,21 @@ puts "Enter your name:"
 user_name = gets.strip
 
 puts say_hello(user_name)
+
+The help command should output instructions for the user on how to use the jukebox.
+The list command should output a list of songs that the user can play.
+The play command should ask a user to input a song name or track number. It should then output the appropriate song name.
+If the user types exit, the jukebox should say goodbye and the program should shut down.
 =end
 
-help 
-list 
-play 
-exit 
+def help
+end
+def list 
+end 
+def play 
+end
+def exit_jukebox
+end 
+def run 
+end 
 
