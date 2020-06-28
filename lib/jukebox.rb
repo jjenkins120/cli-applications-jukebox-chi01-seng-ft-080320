3d9a6f8a27a07cc6417b305535e788f10260498a
Jeff Jenkins <jjenkins120@gmail.com>
2020-06-28 00:32:11 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -47,7 +47,7 @@ def play(songs)
   if (1..9).to_a.include?(user_response.to_i)
     puts "Playing #{songs[user_response.to_i - 1]}"
   elsif songs.include?(user_response.to_s)
-    puts "Playing #{user_response}"""
+    puts "Playing #{user_response}"
   else 
     puts "Invalid input, please try again"
   end 
