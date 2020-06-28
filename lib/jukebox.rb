5373d16945f6ac8cfc06ea36d02712858ea534ee
Jeff Jenkins <jjenkins120@gmail.com>
2020-06-28 00:15:05 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -83,15 +83,13 @@ def run
   chosen_command = gets.strip.to_s
   if chosen_command == "help"
     help
-    run
   elsif chosen_command == "play"
     play
-    run
   elsif chosen_command == "help"
     help
-    run
   elsif chosen_command == "exit"
     exit_jukebox
+    break
   end
 end 
 
